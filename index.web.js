944170742a09237b8684b3e968296a1792ef977b
Pranit Harekar <pranitharekar@hotmail.com>
2019-02-13 10:47:06 -0500

    Initial commit

---
+++ index.web.js
@@ -0,0 +1,14 @@
+/**
+ * @format
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
+ */
+
+import { AppRegistry } from "react-native";
+import App from "./App";
+import { name as appName } from "./app.json";
+
+AppRegistry.registerComponent(appName, () => App);
+
+AppRegistry.runApplication(appName, {
+  rootTag: document.getElementById("root")
+});
